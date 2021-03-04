cdfdad405b46ea7a1496b6cbdcd2e28be6b4b20c
honeyShi <918828350@qq.com>
2021-03-04 16:41:17 +0800

    学习笔记及测试

---
+++ android/app/src/main/kotlin/com/webturing/flutter2/flutter2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webturing.flutter2.flutter2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my first flutter',
+      home: Scaffold(
+        appBar: AppBar(title: Text('adf')),
+        body: Text(
+          'aaa',
+          style: TextStyle(
+            color: Colors.redAccent,
+            fontSize: 50,
+          ),
+
+        ),
+      ),
+    );
+  }
+}
