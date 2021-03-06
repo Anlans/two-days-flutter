0d7ae0bb90ca47caf81eea4b74a676f7e2bd41d4
honeyShi <918828350@qq.com>
2021-03-06 10:37:14 +0800

    第三课Icon

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,34 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'my first flutter',
       home: Scaffold(
         appBar: AppBar(title: Text('adf')),
-        body: Image.asset(
-            'assets/a.png',
-            width: ,
-        ),
+        body: Home(),
       ),
     );
+  }
+}
+class Home extends StatelessWidget {
+  Widget build (BuildContext context) {
+
+    // return FlatButton.icon(
+    //   color: Colors.blue,
+    //   icon: Icon(Icons.favorite, color: Colors.red,),
+    //   label: Text('收藏', style: TextStyle(fontSize: 20, color: Colors.white),),
+    //   onPressed: (){
+    //   },
+    // );
+
+
+    return IconButton(
+      icon: Icon(Icons.favorite, color: Colors.red,),
+      onPressed: (){
+
+      },
+    );
+
   }
 }
